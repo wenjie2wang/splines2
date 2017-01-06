6d3b77884c8086eb22c6ec02711be182abaacc10
Wenjie Wang <wenjie.2.wang@uconn.edu>
2017-01-05 20:38:27 -0500

    add check for knots

--- R/bSpline.R
+++ R/bSpline.R
@@ -88,6 +88,16 @@ bSpline <- function(x, df = NULL, knots = NULL, degree = 3L, intercept = FALSE,
     if ((degree <- as.integer(degree)) < 0)
         stop("'degree' must be a nonnegative integer.")
 
+    ## sort and remove possible NA's in internal knots if exist
+    if (length(knots)) {
+        knots <- sort(knots)
+        tmp <- is.na(knots)
+        if (any(tmp)) {
+            omit <- seq_along(knots)[tmp]
+            knots <- knots[- omit]
+        }
+    }
+
     ## take care of possible NA's in `x` for `Boundary.knots`
     nax <- is.na(x)
     if (all(nax))
