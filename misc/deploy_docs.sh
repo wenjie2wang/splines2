69d66319368b0b93027964d98ae48462f2bc4a7e
Wenjie Wang <wjwang.stat@gmail.com>
2020-07-02 12:53:20 -0400

    remove docs generated by pkgdown from the repository

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -1,21 +1,29 @@
-#/bin/bash
+#!/bin/bash
 
 set -e
 
 ## run on wenjie's droplets
+pkg=$(grep "Package" DESCRIPTION | awk '{print $NF}')
 build_dir=$(pwd)
-cd $HOME/wenjie/wenjie-stat.me/
+docs_repo=$HOME/wenjie/wenjie-stat.me
+target_dir=$docs_repo/static/$pkg
+tmp_log=.git_status.log
 
+## update docs by pkgdown
+make pkgdown
+
+# go to the repository for wenjie-stat.me
+cd $docs_repo
 git checkout -f
 git checkout master
 git pull origin master
-cp -r $build_dir/docs/* $HOME/wenjie/wenjie-stat.me/static/splines2/
-tmp_log=.git_status.log
+mkdir -p $target_dir
+cp -r $build_dir/docs/* $target_dir
 git status > $tmp_log
-if egrep -q "modified:[ ]+docs/" $tmp_log
+if egrep -q "modified:[ ]+static/$pkg/" $tmp_log
 then
-    git add -u static/splines2/
-    git commit -m "deploy splines2 $CI_COMMIT_SHORT_SHA by gitlab-runner"
+    git add -u static/$pkg/
+    git commit -m "deploy $pkg $CI_COMMIT_SHORT_SHA by gitlab-runner"
     git push origin master
 else
     printf "The docs was not updated.\n"
