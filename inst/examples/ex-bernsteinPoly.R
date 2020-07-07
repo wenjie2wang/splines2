94bc55b381e61ed72f97bcf8acf7ec5b01bbef48
Wenjie Wang <wjwang.stat@gmail.com>
2020-07-07 00:04:14 -0400

    update examples

--- inst/examples/ex-bernsteinPoly.R
+++ inst/examples/ex-bernsteinPoly.R
@@ -7,8 +7,7 @@ x2 <- seq.int(- 2, 2, 0.01)
 bMat1 <- bernsteinPoly(x1, degree = 4, intercept = TRUE)
 
 ## generalized Bernstein polynomials basis over [- 1, 2]
-bMat2 <- bernsteinPoly(x2, degree = 4, intercept = TRUE,
-                       Boundary.knots = range(x2))
+bMat2 <- bernsteinPoly(x2, degree = 4, intercept = TRUE)
 
 par(mfrow = c(1, 2), mar = c(2.5, 2.5, 0.2, 0.1), mgp = c(1.5, 0.5, 0))
 matplot(x1, bMat1, type = "l", ylab = "y")
@@ -17,10 +16,8 @@ matplot(x2, bMat2, type = "l", ylab = "y")
 ## the first and second derivative matrix
 d1Mat1 <- bernsteinPoly(x1, degree = 4, derivs = 1, intercept = TRUE)
 d2Mat1 <- bernsteinPoly(x1, degree = 4, derivs = 2, intercept = TRUE)
-d1Mat2 <- bernsteinPoly(x2, degree = 4, derivs = 1, intercept = TRUE,
-                        Boundary.knots = range(x2))
-d2Mat2 <- bernsteinPoly(x2, degree = 4, derivs = 2, intercept = TRUE,
-                        Boundary.knots = range(x2))
+d1Mat2 <- bernsteinPoly(x2, degree = 4, derivs = 1, intercept = TRUE)
+d2Mat2 <- bernsteinPoly(x2, degree = 4, derivs = 2, intercept = TRUE)
 
 par(mfrow = c(2, 2))
 matplot(x1, d1Mat1, type = "l", ylab = "y")
@@ -34,7 +31,6 @@ all.equal(d2Mat1, deriv(bMat1, 2))
 
 ## the integrals
 iMat1 <- bernsteinPoly(x1, degree = 4, integral = TRUE, intercept = TRUE)
-iMat2 <- bernsteinPoly(x2, degree = 4, integral = TRUE, intercept = TRUE,
-                       Boundary.knots = range(x2))
+iMat2 <- bernsteinPoly(x2, degree = 4, integral = TRUE, intercept = TRUE)
 all.equal(deriv(iMat1), bMat1, check.attributes = FALSE)
 all.equal(deriv(iMat2), bMat2, check.attributes = FALSE)
