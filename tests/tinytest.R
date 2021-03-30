961bc1c3c84bb06a5fb7314059e6f82e5ce62abe
Wenjie Wang <wang@wwenjie.org>
2021-03-29 21:13:43 -0400

    Enable additional checks by gha

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -3,11 +3,14 @@ if (requireNamespace("tinytest", quietly = TRUE) &&
 
     ## Set a seed to make the test deterministic
     set.seed(808)
+    is_at_home <- function() {
+        identical(tolower(Sys.getenv("TT_AT_HOME")),"true")
+    }
 
-    ## only run the following tests if not at cron
+    ## only run the following tests if at home (not at cran)
     tinytest::test_package("splines2", ncpu = NULL,
                            test_dir = "rcpp-tests",
-                           at_home = tinytest::at_home())
+                           at_home = is_at_home())
     ## run anyway
     tinytest::test_package("splines2", ncpu = NULL,
                            side_effects = TRUE)
