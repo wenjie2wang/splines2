1aea1610c9eaf19d76cc0fac45b8020d2f531614
Wenjie Wang <wang@wwenjie.org>
2021-08-04 00:11:44 -0400

    Add makepredictcall() methods

---
+++ R/makepredictcall.R
@@ -0,0 +1,108 @@
+##
+## R package splines2 by Wenjie Wang and Jun Yan
+## Copyright (C) 2016-2021
+##
+## This file is part of the R package splines2.
+##
+## The R package splines2 is free software: You can redistribute it and/or
+## modify it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or any later
+## version (at your option). See the GNU General Public License at
+## <https://www.gnu.org/licenses/> for details.
+##
+## The R package splines2 is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+##
+
+## reference: splines:::makepredictcall
+
+##' @importFrom stats makepredictcall
+helper_makepredictcall <- function(var, call, fun, key_attr)
+{
+    fun_symbol <- substitute(fun)
+    fun_name <- as.character(fun_symbol)
+    if (as.character(call)[1L] == fun_name ||
+        (is.call(call) && identical(eval(call[[1L]]), eval(fun_symbol)))) {
+        at <- attributes(var)[key_attr]
+        call <- call[1L:2L]
+        call[names(at)] <- at
+    }
+    call
+}
+
+##' @export
+makepredictcall.bSpline2 <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = bSpline,
+        key_attr = c("degree", "knots", "Boundary.knots", "intercept")
+    )
+}
+
+##' @export
+makepredictcall.naturalSpline <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = naturalSpline,
+        key_attr = c("knots", "Boundary.knots", "intercept",
+                     "derivs", "integral")
+    )
+}
+
+##' @export
+makepredictcall.dbs <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = dbs,
+        key_attr = c("degree", "knots", "Boundary.knots", "intercept", "derivs")
+    )
+}
+
+##' @export
+makepredictcall.ibs <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = ibs,
+        key_attr = c("degree", "knots", "Boundary.knots", "intercept")
+    )
+}
+
+##' @export
+makepredictcall.mSpline <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = mSpline,
+        key_attr = c("degree", "knots", "Boundary.knots", "intercept",
+                     "periodic", "derivs", "integral")
+    )
+}
+
+##' @export
+makepredictcall.iSpline <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = iSpline,
+        key_attr = c("degree", "knots", "Boundary.knots", "intercept", "derivs")
+    )
+}
+
+##' @export
+makepredictcall.cSpline <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = cSpline,
+        key_attr = c("degree", "knots", "Boundary.knots", "intercept",
+                     "derivs", "scale")
+    )
+}
+
+##' @export
+makepredictcall.bernsteinPoly <- function(var, call)
+{
+    helper_makepredictcall(
+        var, call, fun = bernsteinPoly,
+        key_attr = c("degree", "Boundary.knots", "intercept",
+                     "derivs", "integral")
+    )
+}
---
+++ inst/rcpp-tests/test-makepredictcall.R
@@ -0,0 +1,88 @@
+## helper function
+get_predvars <- function(mod, key_attr) {
+    out <- as.list(attr(terms(mod$model), "predvars")[[3]])[key_attr]
+    if (anyNA(names(out)))
+        stop("Found not matched key attribute.")
+}
+get_attr <- function(x, key_attr) {
+    out <- attributes(x)[key_attr]
+    if (anyNA(names(out)))
+        stop("Found not matched key attribute.")
+}
+
+## simulated data
+n <- 1e2
+x <- rnorm(n)
+y <- x + rnorm(n, sd = 0.1)
+new_x <- runif(2 * n, min(x), max(x))
+
+## bSpline()
+mod <- lm(y ~ bSpline(x, df = 6))
+key_attr <- c("degree", "knots", "Boundary.knots", "intercept")
+expect_equal(
+    get_attr(bSpline(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
+
+new_mat <- predict(bSpline(x, df = 6), new_x)
+pred1 <- predict(mod, data.frame(x = new_x))
+pred2 <- coef(mod)[1L] + as.numeric(new_mat %*% coef(mod)[- 1L])
+expect_equivalent(pred1, pred2)
+
+## naturalSpline()
+mod <- lm(y ~ naturalSpline(x, df = 6))
+key_attr <- c("knots", "Boundary.knots", "intercept")
+expect_equal(
+    get_attr(naturalSpline(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
+
+## mSpline()
+mod <- lm(y ~ mSpline(x, df = 6))
+key_attr <- c("degree", "knots", "Boundary.knots", "intercept",
+              "periodic", "derivs", "integral")
+expect_equal(
+    get_attr(mSpline(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
+
+## iSpline()
+mod <- lm(y ~ iSpline(x, df = 6))
+key_attr <- c("degree", "knots", "Boundary.knots", "intercept", "derivs")
+expect_equal(
+    get_attr(iSpline(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
+
+## cSpline()
+mod <- lm(y ~ cSpline(x, df = 6))
+key_attr <- c("degree", "knots", "Boundary.knots", "intercept",
+              "scale", "derivs")
+expect_equal(
+    get_attr(cSpline(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
+
+## dbs()
+mod <- lm(y ~ dbs(x, df = 6))
+key_attr <- c("degree", "knots", "Boundary.knots", "intercept", "derivs")
+expect_equal(
+    get_attr(dbs(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
+
+## ibs()
+mod <- lm(y ~ ibs(x, df = 6))
+key_attr <- c("degree", "knots", "Boundary.knots", "intercept")
+expect_equal(
+    get_attr(ibs(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
+
+## bernsteinPoly()
+mod <- lm(y ~ bernsteinPoly(x, df = 6))
+key_attr <- c("degree", "Boundary.knots", "intercept", "derivs", "integral")
+expect_equal(
+    get_attr(bernsteinPoly(x, df = 6), key_attr),
+    get_predvars(mod, key_attr)
+)
