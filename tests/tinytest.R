529d56a48a4299cfcb693515db92b9c84453b787
Wenjie Wang <wang@wwenjie.org>
2023-03-25 19:49:54 -0400

    Fix the use of at_home()

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -4,10 +4,11 @@ if (requireNamespace("tinytest", quietly = TRUE) &&
     is_at_home <- function() {
         identical(tolower(Sys.getenv("TT_AT_HOME")), "true")
     }
-    ## only run the following tests if "at home" (not at cran)
-    tinytest::test_package("splines2", ncpu = NULL,
-                           testdir = "rcpp-tests",
-                           at_home = is_at_home())
+    if (is_at_home()) {
+        ## only run the following tests if "at home" (not at cran)
+        tinytest::test_package("splines2", ncpu = NULL,
+                               testdir = "rcpp-tests")
+    }
     ## always run the following checks
     tinytest::test_package("splines2", ncpu = NULL,
                            side_effects = TRUE)
