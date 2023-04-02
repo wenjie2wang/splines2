dbf435c1d7867a21e92c29f4e2b4e85a1e64f6d4
Wenjie Wang <wang@wwenjie.org>
2023-04-02 16:37:29 -0400

    Add example of periodic B-splines

--- inst/examples/ex-bSpline.R
+++ inst/examples/ex-bSpline.R
@@ -1,23 +1,37 @@
 library(splines2)
 
-x <- seq.int(0, 1, 0.01)
-knots <- c(0.3, 0.5, 0.6)
+x <- seq(0, 1, 0.01)
+knots <- c(0.3, 0.5, 0.6) # internal knots
 
 ## cubic B-splines
 bsMat <- bSpline(x, knots = knots, degree = 3, intercept = TRUE)
-
-op <- par(mar = c(2.5, 2.5, 0.2, 0.1), mgp = c(1.5, 0.5, 0))
-matplot(x, bsMat, type = "l", ylab = "Cubic B-splines")
-abline(v = knots, lty = 2, col = "gray")
-
-## reset to previous plotting settings
-par(op)
-
-## the first derivaitves
+## the integrals
+ibsMat <- ibs(x, knots = knots, degree = 3, intercept = TRUE)
+## the first derivatives
 d1Mat <- deriv(bsMat)
-
 ## the second derivaitves
 d2Mat <- deriv(bsMat, 2)
 
+op <- par(mfrow = c(2, 2))
+plot(bsMat, main = "Cubic B-splines")
+plot(ibsMat, main = "The integrals")
+plot(d1Mat, main = "The first derivatives")
+plot(d2Mat, main = "The second derivatives")
+par(op) # reset to previous plotting settings
+
 ## evaluate at new values
 predict(bsMat, c(0.125, 0.801))
+
+## periodic B-splines
+px <- seq(0, 3, 0.01)
+pbsMat <- bSpline(px, knots = knots, Boundary.knots = c(0, 1),
+                  intercept = TRUE, periodic = TRUE)
+ipMat <- ibs(px, knots = knots, Boundary.knots = c(0, 1),
+             intercept = TRUE, periodic = TRUE)
+dpMat <- deriv(pbsMat)
+
+op <- par(mfrow = c(1, 3))
+plot(pbsMat, main = "Periodic B-splines", mark_knots = "b")
+plot(ipMat, main = "The integrals", mark_knots = "b")
+plot(dpMat, main = "The first derivatives", mark_knots = "b")
+par(op) # reset to previous plotting settings
