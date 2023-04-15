c0f2dcd4d64628f115c78f337a81c429132afc4a
Wenjie Wang <wang@wwenjie.org>
2023-04-14 20:59:40 -0400

    Revise the plot method following the curve function

--- R/plot.R
+++ R/plot.R
@@ -23,6 +23,9 @@
 ##'
 ##' @param x A \code{splines2} object.
 ##' @param y An argument that is not used.
+##' @param from,to Two numbers representing the start and end point for the
+##'     plot, respectively.
+##' @param n An integer, the number of x values at which to evaluate.
 ##' @param mark_knots A character vector specifying if knot placement should be
 ##'     indicated by vertical lines.
 ##' @param ... Additional arguments (other than \code{x} and \code{y}) that
@@ -30,16 +33,23 @@
 ##'
 ##' @importFrom graphics matplot abline
 ##' @export
-plot.splines2 <- function(x, y,
+plot.splines2 <- function(x, y, from = NULL, to = NULL, n = 101,
                           mark_knots = c("none", "internal", "boundary", "all"),
                           ...)
 {
     dots <- list(...)
     x_ <- attr(x, "x")
+    if (is.null(from)) {
+        from <- min(x_, na.rm = TRUE)
+    }
+    if (is.null(to)) {
+        to <- max(x_, na.rm = TRUE)
+    }
+    x_seq <- seq.int(from = from, to = to, length.out = n)
     default_args <- list(type = "l", xlab = "x", ylab = "")
     call_args <- modify_list(default_args, dots)
-    call_args$x <- x_
-    call_args$y <- x
+    call_args$x <- x_seq
+    call_args$y <- predict(x, newx = x_seq)
     do.call(graphics::matplot, call_args)
     mark_knots <- match.arg(mark_knots,
                             choices = c("none", "internal", "boundary", "all"))
@@ -48,7 +58,7 @@ plot.splines2 <- function(x, y,
         ik <- knots(x, "internal")
         bk <- knots(x, "boundary")
         dist_bk <- bk[2] - bk[1]
-        range_x <- range(x_)
+        range_x <- range(x_seq)
         k1 <- (bk[1] - range_x[1]) %/% dist_bk
         k2 <- (range_x[2] - bk[1]) %/% dist_bk
         shifts <- seq.int(k1, k2) * dist_bk
