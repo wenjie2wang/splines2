068b0ccf8642beaf57157bac837ecc0d1705f35b
Wenjie Wang <wang@wwenjie.org>
2023-04-30 09:12:02 -0400

    Add $ method to extract attribute for convenience

---
+++ R/extract.R
@@ -0,0 +1,21 @@
+##
+## R package splines2 by Wenjie Wang and Jun Yan
+## Copyright (C) 2016-2023
+##
+## This file is part of the R package splines2.
+##
+## The R package splines2 is free software: You can redistribute it and/or
+## modify it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or any later
+## version (at your option). See the GNU General Public License at
+## <https://www.gnu.org/licenses/> for details.
+##
+## The R package splines2 is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+##
+
+##' @export
+`$.splines2` <- function(x, name) {
+    attr(x, name, exact = FALSE)
+}
