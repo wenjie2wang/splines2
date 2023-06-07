bf79d4bd9c57250074b39ce07c8ba37ca739a2df
Wenjie Wang <wang@wwenjie.org>
2023-06-06 23:08:04 -0400

    Add a linear model example for nsk()

--- inst/examples/ex-naturalSpline.R
+++ inst/examples/ex-naturalSpline.R
@@ -26,3 +26,18 @@ all.equal(nsMat0, deriv(nsMat1), check.attributes = FALSE)
 all.equal(nsMat2, deriv(nsMat0))
 all.equal(nsMat3, deriv(nsMat2))
 all.equal(nsMat3, deriv(nsMat0, 2))
+
+## a linear model example
+fit1 <- lm(weight ~ -1 + nsk(height, df = 4, intercept = TRUE), data = women)
+fit2 <- lm(weight ~ nsk(height, df = 3), data = women)
+
+## the knots (same for both fits)
+knots <- unlist(attributes(fit1$model[[2]])[c('Boundary.knots', 'knots')])
+
+## predictions at the knot points
+predict(fit1, data.frame(height = sort(unname(knots))))
+unname(coef(fit1)) # equal to the coefficient estimates
+
+## different interpretation when "intercept = FALSE"
+unname(coef(fit1)[-1] - coef(fit1)[1]) # differences: yhat[2:4] - yhat[1]
+unname(coef(fit2))[-1]                 # ditto
