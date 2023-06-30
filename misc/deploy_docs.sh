6a292789108c620b84de83bb069cf72f41624781
Wenjie Wang <wang@wwenjie.org>
2023-06-30 13:03:36 -0400

    Deploy dev docs

--- misc/deploy_docs.sh
+++ misc/deploy_docs.sh
@@ -7,7 +7,6 @@ pkg=$(grep "Package" DESCRIPTION | awk '{print $NF}')
 build_dir=$(pwd)
 docs_repo=$HOME/wenjie/wwenjie.org
 target_dir=$docs_repo/static/$pkg
-tmp_log=.git_status.log
 
 ## update docs by pkgdown
 make pkgdown
@@ -19,14 +18,11 @@ git checkout main
 git pull origin main
 mkdir -p $target_dir
 cp -r $build_dir/docs/* $target_dir
-git status > $tmp_log
-cat $tmp_log
-if egrep -q "modified:[ ]+static/$pkg/" $tmp_log
+git add static/$pkg/
+if [ -n "$(git diff --cached --exit-code)" ]
 then
-    git add static/$pkg/
     git commit -m "deploy $pkg $CI_COMMIT_SHORT_SHA by gitlab-runner"
     git push origin main
 else
     printf "The docs was not updated.\n"
 fi
-rm $tmp_log
